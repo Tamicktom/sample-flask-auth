b974e9c3ede4a64a2fae026e1fe01fdbe9066c54
Tamicktom <rickpokemon38@gmail.com>
2026-01-31 00:00:45 -0300

    OK

--- src/sample-flask-auth/app.py
+++ src/sample-flask-auth/app.py
@@ -1,5 +1,6 @@
 # Libraries imports
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_login import LoginManager
 
 # Local imports
 from database import db
@@ -10,8 +11,16 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
 
+# View Login
+@app.post("/login")
+def login():
+    data = request.json
+
+    return jsonify({"message": "OK"})
 
 @app.get("/hello")
 def hello_world():
