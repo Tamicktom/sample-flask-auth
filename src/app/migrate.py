0791d403ee18a9ca02ba1929fe9557c87f601a24
Tamicktom <rickpokemon38@gmail.com>
2026-02-01 16:00:07 -0300

    Refactor imports in migrate.py for clarity and organization

--- src/app/migrate.py
+++ src/app/migrate.py
@@ -1,13 +1,12 @@
-# Import the database
+# Libraries imports
+from sqlalchemy import inspect, text
+
+# Local imports
 from database import db
-
-# Import the models
 from models.user import User
-
-# import the app from the __init__.py file
 from __init__ import app
 
-# Create all tables
+
 with app.app_context():
     db.create_all()
     print("Tables created successfully")
